293d2c95d4d9f16e67fa18734cf456576371f407
Dfong1 <103082807+Dfong1@users.noreply.github.com>
2023-06-26 22:35:09 -0600

    Pantalla principal administrador

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const abrirMenu = document.querySelector('.abrir-menu')
 const cerrarMenu = document.querySelector('.cerrar-menu')
 const nav = document.querySelector('.nav__alumno')
+const input = document.querySelector('.form-control')
+const nav2 = document.querySelector('.lista__nav')
 
 abrirMenu.addEventListener('click', () =>{
     nav.classList.add("visible")
