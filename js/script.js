64545d29c7940b26c1cbbd10fc1e68c02b9b724e
Dfong1 <103082807+Dfong1@users.noreply.github.com>
2023-07-12 14:37:09 -0600

    Ultimos cambios pantalla alumnos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const abrirMenu = document.querySelector('.abrir-menu')
 const cerrarMenu = document.querySelector('.cerrar-menu')
 const nav = document.querySelector('.nav__alumno')
-const form = document.querySelector('.card')
+const table = document.querySelector('.table__pag')
 
 abrirMenu.addEventListener('click', () =>{
     nav.classList.add("visible")
-    form.classList.add("none")
+    table.classList.add("none")
 })
 
 cerrarMenu.addEventListener('click', () =>{
     nav.classList.remove("visible")
-    form.classList.remove("none")
+    table.classList.remove("none")
 })
